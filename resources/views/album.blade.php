4a5df3f52ad96c3d12661b453586f869f7d3ba1c
ruslanramdani1st <ruslanramdani1st@gmail.com>
2021-10-05 16:16:54 +0700

    Pengulangan div di dalam FOR

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -23,15 +23,18 @@
             <div class="container px-lg-5">
                 <!-- Page Features-->
                 <div class="row gx-lg-5">
-                    <div class="col-lg-6 col-xxl-4 mb-5">
-                        <div class="card bg-light border-0 h-100">
-                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
-                                <h2 class="fs-4 fw-bold">Fresh new layout</h2>
-                                <p class="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                    @for ($i=1; $i <=19; $i++)
+                        <div class="col-lg-6 col-xxl-4 mb-5">
+                            <div class="card bg-light border-0 h-100">
+                                <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+                                    <h2 class="fs-4 fw-bold">Fresh new layout</h2>
+                                    <p class="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endfor
+
                     <div class="col-lg-6 col-xxl-4 mb-5">
                         <div class="card bg-light border-0 h-100">
                             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
